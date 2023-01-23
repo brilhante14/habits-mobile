ece0846fcd26007171271be5f6a4995a8f4187a8
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-01-22 22:40:42 -0300

    feat: Adding libs and utils

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 import { ActivityIndicator, View } from 'react-native';
 
-export default function Loading() {
+export function Loading() {
    return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#09090A' }}>
          <ActivityIndicator color="#7C3AED" />
