e1c8fbe3df17b5191acccc5b55044d475266aafc
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-01-23 22:20:15 -0300

    feat: Adding function to open an habit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { ScrollView, Text, View } from "react-native";
 import { DAY_SIZE, HabitDay } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -10,6 +11,7 @@ const minimumSummaryDateSize = 18 * 8;
 const amoutOfDaysToFill = minimumSummaryDateSize - datesFromYearStart.length;
 
 export function Home() {
+   const { navigate } = useNavigation();
 
    return (
       <View className="flex-1 bg-background px-8 pt-16">
@@ -33,7 +35,12 @@ export function Home() {
          >
 
             <View className="flex-row flex-wrap">
-               {datesFromYearStart.map((date) => (<HabitDay key={date.toISOString()} />))}
+               {datesFromYearStart.map((date) => (
+                  <HabitDay
+                     key={date.toISOString()}
+                     onPress={() => navigate('habit', { date: date.toISOString() })}
+                  />
+               ))}
 
                {amoutOfDaysToFill > 0 &&
                   Array
